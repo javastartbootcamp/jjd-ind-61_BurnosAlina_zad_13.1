fbf1ba085147fc3942f0c2469b390127ab48de76
Alina Burnos <burnosalina@gmail.com>
2023-03-08 10:52:31 +0100

    Głosowanie: Za, przeciw i wstrzymanie się od głosu

--- src/main/java/pl/javastart/voting/Voting.java
+++ src/main/java/pl/javastart/voting/Voting.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 
 public class Voting {
 
+    public static final String HOLD = "w";
+    public static final String FOR = "z";
+    public static final String AGAINST = "p";
+
     public static void main(String[] args) {
         List<String> voters = new ArrayList<>();
 
@@ -27,8 +31,40 @@ public class Voting {
      * Metoda powinna pobrać głos dla każdego przekazanego głosującego i zapisać wyniki głosowania do VotingResult
      */
     VotingResult executeVoting(List<String> voters, Scanner scanner) {
-
-        return null; // to możesz (a nawet powinieneś/powinnaś) zmienić :)
+        List<Vote> voteList = new ArrayList<>();
+        int counter = 0;
+        double forVoteCounter = 0;
+        double holdVoteCounter = 0;
+        double againstVoteCounter = 0;
+        if (voters.size() == 0) {
+            System.out.println(" Nikt nie głosował");
+        } else {
+            do {
+                System.out.println("Jak głosuje " + voters.get(counter));
+                String vote = scanner.nextLine();
+                switch (vote) {
+                    case HOLD -> {
+                        voteList.add(new Vote(voters.get(counter), vote));
+                        holdVoteCounter++;
+                        counter++;
+                    }
+                    case FOR -> {
+                        voteList.add(new Vote(voters.get(counter), vote));
+                        forVoteCounter++;
+                        counter++;
+                    }
+                    case AGAINST -> {
+                        voteList.add(new Vote(voters.get(counter), vote));
+                        againstVoteCounter++;
+                        counter++;
+                    }
+                    default -> System.out.println("Nieprawidłowe oddanie głosu");
+                }
+            } while (counter < voters.size());
+        }
+        return new VotingResult(forVoteCounter, holdVoteCounter, againstVoteCounter, voteList);
     }
-
 }
+
+
+
